382d347c433e8eb4f0f4a7cddea73efebb25eb16
luzilin <744883907@qq.com>
2023-01-18 11:33:34 +0800

    init project4

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,4 +2,22 @@ export default {
 	title: "vitepress-starter",
 	description: "How to use VitePress",
 	base: '/vitepress-starter/',
+	themeConfig: {
+		nav: [
+			{
+				text: "note", link: '/guide/getting-started', activeMatch: "/guide/"
+			}
+		],
+		sidebar: {
+			'/guide/': [
+				{
+					text: "note",
+					collapsible: false,
+					items: [
+						{ text: "Getting-Started", link: "/guide/getting-started" },
+					]
+				}
+			]
+		},
+	}
 }
